cf479286a0155e077568568a9041908da202f989
Abdullah Zahid <zabdullah613@gmail.com>
2025-05-02 00:32:57 +0500

    update backend

---
+++ config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from the .env file in the current directory
+load_dotenv()
+
+#class Config:
+    #MONGO_URI = "mongodb+srv://myuse:okay@cluster0.amzpn.mongodb.net/Virtual-Disease-Detection"  # Fetch the MONGO_URI from the environment variable
+    #print(f"Loaded Mongo URI: {MONGO_URI}")  # Debug line
---
+++ run.py
@@ -0,0 +1,6 @@
+from app import create_app  # Import the create_app function from the app package
+
+app = create_app()  # Create the Flask app by calling the create_app function
+
+if __name__ == '__main__':  # Check if this script is being run directly (as opposed to being imported)
+    app.run()  # Run the Flask app locally
